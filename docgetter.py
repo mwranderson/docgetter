ecbdb59a8ba32700befc1c7bdf1faa5c2b54939f
esfandiar <esfandiar.rouhani@gmail.com>
2024-04-08 18:41:45 -0500

    repeat processing patch 1

--- docgetter.py
+++ docgetter.py
@@ -1,11 +1,16 @@
+from threading import Thread
 import slack_sdk as slack
-from flask import Flask, Response, request
+from flask import Flask, request, jsonify
 import os
+from waitress import serve
 from modules.get_report import getreport
 from dotenv import load_dotenv 
-#from requests import request
+from slackeventsapi import SlackEventAdapter
 
 load_dotenv() 
+
+# slack ID of RP in charge -- currently Esfandiar
+RP_ID = '<@U01KCEYLA85>'
  
 SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 SIGNING_SECRET = os.getenv("SIGNING_SECRET")
@@ -13,54 +18,104 @@ SIGNING_SECRET = os.getenv("SIGNING_SECRET")
 app = Flask(__name__)
 client = slack.WebClient(token=SLACK_TOKEN)
 
-@app.route('/', methods=['GET', 'POST']) 
-def hey_slack():
-    message = request.get_json()
-    print('\n\nAHH THIS IS IT, MAN:\n\n')
-    print(message)
-    """ if request_json.get("challenge") is not None:
-        return Response(request_json.get("challenge"), status=200) """
-    return Response(status=200)
+slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, endpoint="/slack/events")
 
-@slack_event_adapter.on("app_mention")
-def handle_message(event_data):
-    def send_reply(value):
-        event_data = value
-        print(event_data)
-        event = event_data.get('event', {})
-        channel_id = event.get('channel')
-        user_id = event.get('user')
-        ts = event.get('ts')
-        thread_ts = event.get('thread_ts')
-        if thread_ts:
-            ts = thread_ts
-        text = event.get('text')
-        if "get report" in text and (len(text.split(' ')) == 4):
-            report_id = text.split(' ')[-1]
-            if not report_id.isnumeric():
-                client.chat_postMessage(channel=channel_id, text=f'Invalid report number. Try again.', thread_ts=ts)
-            else:
-                report_id = int(report_id)
-                client.chat_postMessage(channel=channel_id, text=f'Getting report {report_id}...', thread_ts=ts)
-                result = getreport(report_id)
-                if result[2]:
-                    client.chat_postMessage(channel=channel_id, text=f'Report exists in multiple pdf files. \nFile: "{result[2]}" chosen at random.', thread_ts=ts)
-                if not result[0]: #something went wrong. Print relevant message.
-                    client.chat_postMessage(channel=channel_id, text=str(result[1]), thread_ts=ts)
-                else:
-                    client.files_upload_v2(channel=channel_id,
-                            initial_comment="Here's the report:",
-                            file=f'{os.getcwd()}/tempdir/{result[1]}', 
-                            thread_ts = ts)
-                    os.remove(f'{os.getcwd()}/tempdir/{result[1]}')
-        else:
-            client.chat_postMessage(channel=channel_id, text='Invalid command. Type "get report" followed by report number.', thread_ts=ts)
-    thread = Thread(target=send_reply, kwargs={"value": event_data})
+# check if a slack json request is of a given type
+def check_request_type(request, type):
+    # get event
+    event = request.get('event')
+    if event:
+        # get type
+        event_type = event.get('type')
+        # check if type is given and that it matches given type
+        if event_type and event_type == type:
+            return True
+
+# basic get handler
+@app.route('/', methods=['GET'])
+def hey_slack():
+    return jsonify({'message': 'Hello world!'})
+
+# handle all incoming post traffic to end point
+@app.route('/slack/events', methods=['POST'])  # type: ignore
+def verify_slack():
+    # convert to json
+    message = request.get_json()
+
+    # return challenge if there is one
+    challenge = message.get('challenge') #type: ignore
+    if challenge:
+        # create json response
+        res = jsonify({'challenge': challenge})
+        # set content type 
+        res.headers['Content-Type'] = 'application/json'
+        # return response
+        return res
+    
+    # if not challenge, handle request
+
+    thread = Thread(target=handle_message, kwargs={"event_data": message})
     thread.start()
-    return Response(status=200)
+    return {'message': 'succesful request'}, 200
+
+def handle_message(event_data):
+   
+    # Only continue if it's an app mention
+    if not check_request_type(event_data, 'app_mention'):
+        print(f'Not an app_mention.')
+        # return 400 with message
+        return {'message': 'request is not of valid type. Currently only listening to app mentions.'}, 400
+
+    # get event data
+    event = event_data.get('event', {})
+    # get channel id to respond properly
+    channel_id = event.get('channel')
+    # get message information to repl
+    ts = event.get('ts')
+    # get thread information to reply in thread if needed
+    thread_ts = event.get('thread_ts')
+    if thread_ts:
+        ts = thread_ts
+    # get app mention request text
+    text = event.get('text')
+    # confirm request syntax
+    if "get report" in text and (len(text.split(' ')) == 4):
+        # get document report id
+        report_id = text.split(' ')[-1]
+        # defend agaisnt non numeric report id
+        if not report_id.isnumeric():
+            client.chat_postMessage(channel=channel_id, text=f'Invalid report number. Try again.', thread_ts=ts)
+        else:
+            # convert report id to int
+            report_id = int(report_id)
+            # send progress message
+            client.chat_postMessage(channel=channel_id, text=f'Getting report {report_id}...', thread_ts=ts)
+            # try and get report 
+            try:
+                result = getreport(report_id)
+            except Exception as e:
+                # return error reason
+                client.chat_postMessage(channel=channel_id, thread_ts = ts, text=f'Report not found due to {e}\nRequires manual intervention {RP_ID}.')
+                return
+            
+            # handle if there were multiple names found -- occurs due to oddities in old pdf downloads
+            if result[2]:
+                client.chat_postMessage(channel=channel_id, text=f'Report exists in multiple pdf files. \nFile: "{result[2]}" chosen at random.', thread_ts=ts)
+            # handle some kind of error
+            if not result[0]: 
+                client.chat_postMessage(channel=channel_id, text=str(result[1]), thread_ts=ts)
+            # all went well -- upload files from tempdir and delete it
+            else:
+                client.files_upload_v2(channel=channel_id,
+                        initial_comment="Here's the report:",
+                        file=f'{os.getcwd()}/tempdir/{result[1]}', 
+                        thread_ts = ts)
+                os.remove(f'{os.getcwd()}/tempdir/{result[1]}')
+    else:
+        client.chat_postMessage(channel=channel_id, text='Invalid command. Type "get report" followed by report number.', thread_ts=ts)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    serve(app, port=port)
-    #app.run(debug=True)
+    #serve(app, port=port)
+    app.run(debug=True)
     
--- modules/get_report.py
+++ modules/get_report.py
@@ -10,71 +10,126 @@ from io import StringIO
 load_dotenv() 
 warnings.simplefilter(action='ignore', category=UnicodeWarning)
 
+# slack ID of RP in charge -- currently Esfandiar
+RP_ID = '<@U01KCEYLA85>'
+
 #ssh into mercury using paramiko
 host = 'mercury.chicagobooth.edu'
 username = 'erouhani'
+## add .env file to main folder that contains your mercury sshkey
 key = pk.RSAKey.from_private_key(StringIO(str(os.environ.get("MERCURY_KEY"))))
 
+# dataset of all reports with column set up to download from mercury directory structure
 DF = pd.read_csv('./trans_ref.csv')
 
 def pdf_helper(report, file):
+    ''' Old pdf files are saved as bundles. \\ 
+    Given report number and address to master pdf bundle file, \\ 
+    cuts out that report and returns it as its own PDF.'''
+    # open master pdf bundle
     reader = PdfReader("/tmp/"+file)
+    # get number of pages
     number_of_pages = len(reader.pages)
+    # initialize variables needed cut up pages
     break_point = False
     match = False
     page_range = ''
+    # go through pages
     for i in range(number_of_pages):
+        # exit condition
         if break_point:
             break
+        # read page
         page = reader.pages[i]
+        # get page text
         text = page.extract_text()
+        # get page lines
         lines = text.split('\n')
+        # go through lines
         for j, line in enumerate(lines):
+            # if report number is found, get page range of report
+            # this is typically in the batch pdf's table of contents
             if str(report) in line:
+                # line where report number is found
                 match = line
+                # get that line and the one after 
+                # -- page range takes up two lines
                 page_range = lines[j+1]
+                # set exit condition
                 break_point = True
                 break
+    # if no match found, exit
     if not match:
         return False
+    
+    # get first page of report
     first_page = int(page_range.split(' ')[0])
+    # get last page of report
     last_page = int(page_range.split(' ')[-1])
 
+    # set up pdf object
     writer = PdfWriter()
+    # add pages to pdf object
     for i in range(first_page-1, last_page):
         writer.add_page(reader.pages[i])
+    # save pdf file to local directory
     with open(f"/tmp/{report}.pdf", "wb") as fp:
         writer.write(fp)
-
+    
+    # return pdf
     return f"{report}.pdf"
 
 def getreport(report, local_dir = None):
     """
-    local_dir can be used when function is used outside app
+    Given report number, finds it in mercury and saves it to temporary directory.\\
+    This function can be used outside the context of the app, in which case \\
+    local_dir determines the output folder for saving report files. 
     """
+    # initialize batch pdf file variable
     multipdf_filename = False
-    # decide which folder it's in
+    # decide which folder it's in using dataset
     sub = DF[DF.report == report]
+    
+    # if not found in dataset
     if not sub.shape[0]:
         print('Report does not exist in dataset. Verify report number and try again.')
         return [False, 'Report does not exist in dataset. Verify report number and try again.', multipdf_filename]
+    
+    # if multiple different matches found with report number -- should be relatively rare
+    # this is why it requires manual intervention
     if (len(set(sub.transcript_source)) > 1) or (len(set(sub.date)) > 1):
-        print('Too many options. Requires manual intervention. <@U01KCEYLA85>.')
-        return [False, 'Too many options. Requires manual intervention. <@U01KCEYLA85>.', multipdf_filename]
+        print(f'Too many options. Requires manual intervention. {RP_ID}.')
+        return [False, f'Too many options. Requires manual intervention. {RP_ID}.', multipdf_filename]
+    
+    # get filenames of report
     filenames = sub.file_name.to_list()
+
+    # if multiple pdfs, due to oddities with old pdf downloads, pick randomly.
+    # manual inspection showed that these are effectively identical. 
     if len(filenames) > 1:
         print('Multipdf problem. One PDF picked at random.')
         multipdf_filename = filenames[0]
+    
     print(f'{report=}')
+
+    # get filename
     filename = filenames[0]
+    # get transcript source
+    # 0: refinitiv pdf
+    # 1: refinitiv xml
+    # 2: factset xml
+    # 3: capital IQ
     transcript_source = sub.transcript_source.values[0]
     print(f'{transcript_source=}')
+    # get event_date, date, and year
     event_date = pd.to_datetime(sub.date)
     date = pd.DatetimeIndex(event_date).date[0]
     print(f'{date=}')
     year = pd.DatetimeIndex(event_date).year[0]
     print(f'{year=}')
     
+    ## get file path -- logic follows organization on Mercury
+    # refinitiv pdf case
     if transcript_source == 0:
         if date <= datetime.date(2021, 4, 5):
             directory = "/project/kh_mercury_1/conference_call/pdf_files/output/01_download_cc/01.1_pdf/20010101-20210405"
@@ -82,37 +137,48 @@ def getreport(report, local_dir = None):
             directory = "/project/kh_mercury_1/conference_call/pdf_files/output/01_download_cc/01.1_pdf/20201001-20210909"
         else:
             directory = "/project/kh_mercury_1/conference_call/pdf_files/output/01_download_cc/01.1_pdf/20210101-20220617"
+    # refinitiv xml case
     elif transcript_source == 1:
-        directory = "/project/kh_mercury_1/refinitiv_univ/TRANSCRIPT/XML_Add_IDs/Archive/"+str(year)
-        if date >= datetime.date(2023, 1, 1):
-            directory = directory + '/2023_firsthalf'
+        directory = f"/project/kh_mercury_1/refinitiv_univ/TRANSCRIPT/XML_Add_IDs/Archive/{year}"
+    # factset xml case
     elif transcript_source == 2:
-        directory = "/project/FactSet/fdsloader/unzipped_data/"+str(year)
+        directory = f"/project/FactSet/fdsloader/unzipped_data/tr_history_{year}"
+    # capital IQ case
+    elif transcript_source == 3:
+        # IMPLEMENT CAPITALIQ LOGIC
+        print(f'CapitalIQ retrieval not yet implemented. Requires manual intervention. {RP_ID}.')
+        return [False, f'CapitalIQ retrieval not yet implemented. Requires manual intervention. {RP_ID}.', multipdf_filename]
     else:
-        print('Invalid transcript source. Requires manual intervention. <@U01KCEYLA85>.')
-        return [False, 'Invalid transcript source. Requires manual intervention. <@U01KCEYLA85>.', multipdf_filename]
+        print(f'Invalid transcript source. Requires manual intervention. {RP_ID}.')
+        return [False, f'Invalid transcript source. Requires manual intervention. {RP_ID}.', multipdf_filename]
     
-    # get file from mercury
+    ## download file from mercury
     # log into mercury
     print(f'Logging into mercury to get {directory}/{filename}\n')
     ssh = pk.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(pk.AutoAddPolicy())
     ssh.connect(host, username=username, pkey=key)
+    
+    # open mercury connection
     sftp = ssh.open_sftp()
-    # ssh connected getting file
+    # get file
     if local_dir:
+        # if download was local
         sftp.get(f'{directory}/{filename}', f'{local_dir}/{filename}')
     else:
+        # if download was via slack
         sftp.get(f'{directory}/{filename}', f'/tmp/{filename}')
+    # close mercury connection
     sftp.close()
+
+    # process pdf batch file if necessary
     if transcript_source == 0:
+        # process and get filename for modified pdf file
         filename = pdf_helper(report, filenames[0])
+        # handle file not found.
         if not filename:
             print('Problem with pdf search. Report not found in large pdf.')
             return [False, 'Problem with pdf search. Report not found in large pdf.', multipdf_filename]
-    else:
-        filename = filenames[0]
-        print(f'{filename=}')
    
     return [True, filename, multipdf_filename]
